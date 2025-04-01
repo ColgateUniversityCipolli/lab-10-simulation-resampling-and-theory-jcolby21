d4727411ade639656b3e1408feefeb4dfadb922e
Jackson Colby <jcolby@colgate.edu>
2025-04-01 14:22:40 -0400

    2 Resampling done

--- Lab10.R
+++ Lab10.R
@@ -45,3 +45,31 @@ margin_of_error_2 <- diff(range_95_2) / 2
 
 
 #Resampling
+
+n_survey <- 1000  # Sample size for Gallup survey 
+#create survey data where 39% satisfied (1), rest are not (0)
+survey_data <- c(rep(1, 0.39 * n_survey), rep(0, n_survey - 0.39 * n_survey))
+
+num_resamples <- 10000  # Number of resamples
+resample_proportions <- numeric(num_resamples)  # Initialize a vector to store proportions
+
+for (i in 1:num_resamples) {
+  resample <- sample(survey_data, n_survey, replace = TRUE)  # Resample with replacement
+  resample_proportions[i] <- mean(resample)
+}
+
+df_resamples <- tibble(proportion = resample_proportions)
+
+ggplot(df_resamples, aes(x = proportion)) +
+  geom_histogram(aes(y = after_stat(density)), bins = 30, fill = "lightblue", color = "white", alpha = 0.7) +
+  geom_density(color = "red", linewidth = 1.2) +
+  labs(title = "Sampling Distribution of Proportion (Resampling)",
+       x = "Sample Proportion",
+       y = "Density") +
+  theme_minimal()
+
+# Middle 95% range from resampling
+range_95_resample <- quantile(df_resamples$proportion, c(0.025, 0.975))
+margin_of_error_resample <- diff(range_95_resample) / 2
+#Middle 95% was from 0.36 to 0.42
+#Margin of error is approximately 0.03, Gallup reported 4% (0.04)
